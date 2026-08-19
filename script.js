7731747e7f4c4fc87e42fd3396cb2f147fd0008b
Viresh Ranjanagi <167700794+veereshr4446@users.noreply.github.com>
2026-08-19 12:38:45 +0530

    Add form submission handler with alert and reset

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thanks for reaching out! I\'ll get back to you soon.');
+    this.reset();
+});
